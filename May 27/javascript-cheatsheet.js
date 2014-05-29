88401bb219fd45c4a4f3d2a01bafdb9a4891d150
Christopher Mehmed <chrismehmed@gmail.com>
2014-05-29 14:55:06 -0400

    layout HW

---
+++ May 27/javascript-cheatsheet.js
@@ -0,0 +1,32 @@
+// JavaScript Cheatsheet
+
+// a. Variables
+
+// b. Debugging
+
+	// alerts
+
+	// comments
+
+	// the console
+
+// c. Data Types
+
+	//
+
+// d. Arrays
+
+// e. Testing
+
+// f. Logic
+
+// g. Functions
+
+// h. Linking to an html file
+
+/** 
+1) 	Write 3 different functions that take input and returns 
+	something via the console or with and alert.
+2)	Make a function with if then / else if / else.
+**/
+

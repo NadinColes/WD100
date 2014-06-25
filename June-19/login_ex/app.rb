15281afcd7bbc2389762c9bf3273ba0f7a4c574d
Christopher Mehmed <chrismehmed@gmail.com>
2014-06-24 21:09:27 -0400

    June 24 update

--- June-19/login_ex/app.rb
+++ June-19/login_ex/app.rb
@@ -4,9 +4,40 @@ require 'sinatra'
 require 'sinatra/activerecord'
 
 set :database, "sqlite3:project_database.sqlite3"
+set :sessions, true
 
 require './models'
 
-get '/' do 
-	@User.find(1)
+helpers do
+	def current_user
+		if session[:user_id] = @user_id
+		end
+	end
 end
+
+get '/' do 
+	flash[]
+	"Hello World"
+end
+
+# get '/sign_in' do
+# 	erb :sign_in
+# end
+
+post '/sign_in' do   
+	@user = User.where(username: params[:username]).first   
+	if @user.password == params[:password] 
+	session[:user_id] = user.id   
+		redirect "/"
+	else     
+		redirect "/login-failed"   
+	end 
+end
+
+=begin
+
+A User model with at least a fname, lname, email, and password column
+
+A login form for the user that tells them if they’ve signed in successfully or not upon being submitted using flash messaging
+
+=end

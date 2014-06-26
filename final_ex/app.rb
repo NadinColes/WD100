49aac4b0eff7786e35130f398aec313ab6d2f3aa
Christopher Mehmed <chrismehmed@gmail.com>
2014-06-26 19:20:44 -0400

    login working in erb

--- final_ex/app.rb
+++ final_ex/app.rb
@@ -7,14 +7,33 @@ set :database, "sqlite3:project_database.sqlite3"
 
 require './models'
 
+
 get '/' do
 	erb :Welcome
 end
 
+
+get '/user_not_found' do
+	erb :user_not_found
+end
+
+
+get '/success' do
+	erb :success
+end
+
+
+get '/login_failed' do
+	erb :login_failed
+end
+
+
 post '/sign_in' do 
 	@user = User.where(email: params[:email]).first
-	if @user.password == params[:password] 
-		redirect "/"   
+	if !@user
+		redirect "/user_not_found"
+	elsif @user.password == params[:password] 
+		redirect "/success"   
 	else 
 		redirect "/login_failed"
 	end
